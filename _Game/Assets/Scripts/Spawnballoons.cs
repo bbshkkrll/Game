bab59ca4713f610aa94521e10f7a9229004f1ff7
Kirill Bobeshko <bobeshko@icloud.com>
2021-04-14 22:22:38 +0500

    edit spawn time

--- _Game/Assets/Scripts/Spawnballoons.cs
+++ _Game/Assets/Scripts/Spawnballoons.cs
@@ -6,7 +6,7 @@ public class Spawnballoons : MonoBehaviour
 {
     public GameObject[] balloons;
     private float[] positions = {-8f, -6f, -4f, -1f, 0, 1f, 3f, 5f, 7f, 8f};
-    public float time = 1f;
+    public float time = 0.75f;
     void Start()
     {
         StartCoroutine(SpawnRedBalloons());
