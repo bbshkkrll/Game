dfd47f49fa9ac481382100b0f0bd88c890309fa4
Kirill Bobeshko <bobeshko@icloud.com>
2021-04-14 18:44:51 +0500

    Add a Balloons-spawn script

--- _Game/Assets/Scripts/MoveObjects.cs
+++ _Game/Assets/Scripts/MoveObjects.cs
@@ -6,22 +6,13 @@ using Random = System.Random;
 
 public class MoveObjects : MonoBehaviour
 {
-    private Random rnd = new Random();
-    public GameObject redBalloon;
-    private float nextFloat;
     public float speed = 2f;
-    private void Start()
-    {
-        nextFloat = rnd.Next(-10,10);
-    }
-
     public void FixedUpdate()
     {
         transform.Translate(Vector3.up * (speed * Time.deltaTime));
 
         if (transform.position.y > 7.5f)
         {
-            Instantiate(redBalloon, new Vector3(nextFloat, -7.5f, 0), Quaternion.identity);
             Destroy(gameObject);
         }
     }
