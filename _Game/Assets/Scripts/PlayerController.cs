5e0d5ca50d0b27e8f5bf866e9d16961e66462a41
Kirill Bobeshko <bobeshko@icloud.com>
2021-04-15 20:19:13 +0500

    Add player controller and box collider

---
+++ _Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+//float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 dir = new Vector3(horizontal, 0, 0);
+        transform.Translate(dir.normalized * Time.deltaTime * speed);
+    }
+}
