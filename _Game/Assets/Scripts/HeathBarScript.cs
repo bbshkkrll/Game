1e252b7be48950bd5ed0aa6c05fd164d70edd48f
Kirill Bobeshko <bobeshko@icloud.com>
2021-04-27 20:55:42 +0500

    temp

---
+++ _Game/Assets/Scripts/HeathBarScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeathBarScript : MonoBehaviour
+{
+    public Slider slider;
+    // Start is called before the first frame update
+    public void SetHealth(int health)
+    {
+        slider
+    }
+}
