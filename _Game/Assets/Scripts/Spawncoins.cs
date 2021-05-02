88ca38afc3c45a5f979429e31bdd63ff547c3304
ksil0Q <vip.mark7373@mail.ru>
2021-05-03 00:07:55 +0300

    add money and their spawn, needs to finish

---
+++ _Game/Assets/Scripts/Spawncoins.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawncoins : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject coins;
+    float RandX;
+    Vector2 whereToSpawn;
+    [SerializeField]
+    public float time = 0.75f;
+    private float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+    void Start()
+    {
+        StartCoroutine(SpawnCoins());
+    }
+    IEnumerator SpawnCoins()
+    {
+        while (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            RandX = Random.Range(-8f, 8f);
+            whereToSpawn = new Vector2(RandX, transform.position.y);
+            Instantiate(coins, whereToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(time);
+        }
+    }
+
+    //нахуевертил сам хз чего, починю 4 мая вечером, чмок
+    void Update()
+    { 
+        
+    }
+}
