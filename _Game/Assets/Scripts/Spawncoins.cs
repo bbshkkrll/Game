ef5cc7754529839b54e23e35ab00ddf36e62ea37
ksil0Q <vip.mark7373@mail.ru>
2021-05-03 21:56:42 +0300

    naconecto coins has been finished

--- _Game/Assets/Scripts/Spawncoins.cs
+++ _Game/Assets/Scripts/Spawncoins.cs
@@ -5,39 +5,26 @@ using UnityEngine;
 public class Spawncoins : MonoBehaviour
 {
     [SerializeField]
-    private GameObject coins;
-    float RandX;
-    Vector2 whereToSpawn;
+    public GameObject[] coins;
     [SerializeField]
-    //public float time = 0.75f;
-    private float spawnRate = 2f;
-    float nextSpawn = 0.0f;
+    public float time = 3f;
     void Start()
     {
-        //StartCoroutine(SpawnCoins());
+        StartCoroutine(SpawnCoins());
     }
-  /*  IEnumerator SpawnCoins()
+    IEnumerator SpawnCoins()
     {
-        while (Time.time > nextSpawn)
+        while (true)
         {
-            nextSpawn = Time.time + spawnRate;
-            RandX = Random.Range(-8f, 8f);
-            whereToSpawn = new Vector2(RandX, transform.position.y);
-            Instantiate(coins, whereToSpawn, Quaternion.identity);
+            Instantiate(coins[Random.Range(0, coins.Length - 1)],
+                new Vector3(Random.Range(-8f, 8f), -5.5f, 0),
+                Quaternion.identity);
             yield return new WaitForSeconds(time);
         }
-    }*/
+    }
 
-    //нахуевертил сам хз чего, починю 4 мая вечером, чмок
     void Update()
     {
-        if (Time.time > nextSpawn)
-        {
-            nextSpawn = Time.time + spawnRate;
-            RandX = Random.Range(-8f, 8f);
-            whereToSpawn = new Vector2(RandX, transform.position.y);
-            Instantiate(coins, whereToSpawn, Quaternion.identity);
-            
-        }
+
     }
 }
