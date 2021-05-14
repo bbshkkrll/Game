57a6efc0de1ece22b681bb38ed2aea54d70a8afd
Kirill Bobeshko <bobeshko@icloud.com>
2021-05-14 19:25:06 +0500

    add  a half part of end level

---
+++ _Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private bool gameHasEnded = false;
+    
+    public float restartDelay = 1f;
+
+    public GameObject compliteLevelUI;
+    public void CompleteLevel()
+    {
+        compliteLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
