f50abf9972cde69f047a0ee794f96cc6e0b4eb13
ksil0Q <vip.mark7373@mail.ru>
2021-05-14 18:01:39 +0300

    add a score

--- _Game/Assets/Scripts/Player.cs
+++ _Game/Assets/Scripts/Player.cs
@@ -6,16 +6,20 @@ using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    public int maxHealt = 100;
+    public int maxHealth = 100;
     public int currentHealth;
     public HealthBar healthBar;
-        
-        
+    private float attackRate = 1f;
+    private float nextAttackTime = 0f;
+
+
+
+
     public float speed = 5f;
 
     private void Start()
     {
-        currentHealth = maxHealt;
+        currentHealth = maxHealth;
     }
 
     void Update()
@@ -25,23 +29,32 @@ public class Player : MonoBehaviour
 
         var dir = new Vector3(horizontal, vertical, 0);
         transform.Translate(dir.normalized * (Time.deltaTime * speed));
+        
     }
 
     private void OnCollisionStay2D(Collision2D collision2D)
     {
-        if (collision2D.gameObject.CompareTag("Bird"))
+        if (collision2D.gameObject.CompareTag("Bird") && Time.time >= nextAttackTime)
         {
-            TakeDamage(1);
-            //Destroy(collision2D.gameObject);
+            TakeDamage();
+            nextAttackTime = Time.time + 1f / attackRate;
         }
     }
 
-    void TakeDamage(int dmg)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bird"))
+            TakeDamage();
+    }
+
+    void TakeDamage()
     {
         healthBar.SetHealth(currentHealth);
-        
-        currentHealth -= dmg;
+        currentHealth --;
     }
+
+
+
     /*private void OnCollisionStay(Collision collision)
     {
         if (collision.gameObject.CompareTag("Balloon"))
